81f1077457a10f0e2a3851e60cfbd2a57d0ef336
moha rhati <aichoun026@gmail.com>
2021-11-27 19:49:06 +0100

    cors3

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: '*'
+        origin: ['http://127.0.0.1:5500', 'https://captainmoh.github.io/locationCar']
     })
 );
 app.use(express.json());
