4ff0b0edde4776b53d35e88cd5720a6ce40ce7a2
moha rhati <aichoun026@gmail.com>
2021-12-04 00:46:03 +0100

    dotenv

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect('mongodb+srv://' + DB_USER_PASS + '@cluster0.drert.mongodb.net/dbVoiture', 
+    .connect('mongodb+srv://' + process.env.DB_USER_PASS + '@cluster0.drert.mongodb.net/dbVoiture', 
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
